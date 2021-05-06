191d68db16d4cff6a3c48c941859da99c261b06e
Crowdbotics <team@crowdbotics.com>
2021-05-06 17:44:15 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/noisy_breeze_26265/MainActivity.java
@@ -0,0 +1,15 @@
+package com.noisy_breeze_26265;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "noisy_breeze_26265";
+  }
+}
